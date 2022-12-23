82a5f4962772982424c8d5d5dbc0179d84709089
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-12-23 12:50:53 +0530

    Update Dataframe.py

--- Assignment 1/Dataframe.py
+++ Assignment 1/Dataframe.py
@@ -5,16 +5,16 @@ import pandas as pd
 # Create an empty data frame with column names
 df = pd.DataFrame(columns=['Name', 'Age', 'Percentage'])
 # Add records
-df.loc[0] = ['Rhutik', '20', 87]
+df.loc[0] = ['Rhutik', '21', 87]
 df.loc[1] = ['Sagar', '19', 69]
-df.loc[2] = ['Sahil', '18', 76]
+df.loc[2] = ['Sahil', '21', 76]
 df.loc[3] = ['Gaurav', '20', 92]
 df.loc[4] = ['Sayali', '21', 85]
-df.loc[5] = ['Rutuja', '22', 93]
-df.loc[6] = ['Supriya', '25', 62]
-df.loc[7] = ['Sonali', '17', 50]
-df.loc[8] = ['Sarthak', '16', 52]
-df.loc[9] = ['Sakshi', '23', 98]
+df.loc[5] = ['Rutuja', '20', 93]
+df.loc[6] = ['Supriya', '23', 62]
+df.loc[7] = ['Sonali', '16', 50]
+df.loc[8] = ['Sarthak', '19', 52]
+df.loc[9] = ['Sakshi', '21', 98]
 
 # Print the dataframe
 print(df)
