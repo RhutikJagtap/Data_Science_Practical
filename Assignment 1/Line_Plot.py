ce15f6b29ce0895055c23886d2e37df061bd3db5
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-12-24 16:41:21 +0530

    Update Line_Plot.py

--- Assignment 1/Line_Plot.py
+++ Assignment 1/Line_Plot.py
@@ -1,10 +1,10 @@
-# Set A-7). Write a Python program to generate a line plot of name vs percentage
+# Set A-7). Write a Python program to generate a line plot of name vs Salary
 
 import matplotlib.pyplot as plt
 Name = ['Rhutik', 'Sahil', 'Rutuja', 'Sayali', 'Sakshi']
-Percentage = [50, 56, 60, 85, 90]
-plt.plot(Name, Percentage, color='Blue')
+Salary = [50, 56, 60, 85, 90]
+plt.plot(Name, Salary, color='Blue')
 plt.xlabel('Name')
-plt.ylabel('Percentage')
-plt.title('Names of students with percentage')
+plt.ylabel('Salary')
+plt.title('Names of  with Salary')
 plt.show()
